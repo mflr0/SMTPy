59a819aa2cdf3f51729b82eeb2161c77dbe4a9ad
mflr0 <mflr0@onionmail.org>
2024-03-14 00:09:16 +0100

    Ajout de .env et modification de send_mail() pour inclure le nom de l'expéditeur
    Nouveau theme sombre

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-##
-##  main.py
-##  Python-SMTP
-##
-##  Created by 0xGuigui on 13/03/2024.
-##  Contributor(s): 0xGuigui
-##
-
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -17,14 +9,13 @@ import ttkthemes
 import tkinter.ttk as ttk
 import markdown
 
-
-def send_mail(username, password, to_emails, cc_emails, subject, body):
+def send_mail(username, password, sender_name, to_emails, cc_emails, subject, body):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(username, password)
 
     msg = MIMEMultipart()
-    msg['From'] = username
+    msg['From'] = f"{sender_name} <{username}>"
     msg['To'] = ', '.join(to_emails)
     msg['Subject'] = subject
 
@@ -36,79 +27,80 @@ def send_mail(username, password, to_emails, cc_emails, subject, body):
     server.sendmail(username, all_recipients, msg.as_string())
     server.quit()
 
-
 def submit_form():
     username = username_entry.get()
     password = password_entry.get()
+    sender_name = sender_name_entry.get()  # Added
     to_emails = [email.strip() for email in to_email_entry.get().split(',')]
     cc_emails = []  # Add your BCC emails here
     subject = subject_entry.get()
     body = body_entry.get("1.0", "end-1c")
 
     try:
-        send_mail(username, password, to_emails, cc_emails, subject, body)
+        send_mail(username, password, sender_name, to_emails, cc_emails, subject, body)
         messagebox.showinfo("Success", "Email sent successfully")
         if save_credentials_var.get():
             save_credentials(username, password)
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
-
 def load_credentials():
-    if os.path.exists('credentials.json'):
-        with open('credentials.json', 'r') as f:
+    if os.path.exists('.env'):
+        with open('.env', 'r') as f:
             credentials = json.load(f)
             username_entry.insert(0, credentials['username'])
             password_entry.insert(0, credentials['password'])
 
-
 def save_credentials(username, password):
     credentials = {'username': username, 'password': password}
-    with open('credentials.json', 'w') as f:
+    with open('.env', 'w') as f:
         json.dump(credentials, f)
 
+root = ttkthemes.ThemedTk(theme="equilux")
+root.resizable(False, False)
 
-# Create root window
-root = ttkthemes.ThemedTk(theme="yaru")
+form_frame = ttk.Frame(root, padding=(20, 10))
+form_frame.grid(row=0, column=0, sticky='ew')
 
-# Create widgets
-username_label = ttk.Label(root, text="Email:")
-username_label.grid(row=0, column=0, padx=10, pady=10)
-username_entry = ttk.Entry(root)
-username_entry.grid(row=0, column=1, padx=10, pady=10)
+button_frame = ttk.Frame(root, padding=(0, 10))
+button_frame.grid(row=1, column=0, sticky='ew')
 
-password_label = ttk.Label(root, text="Password:")
-password_label.grid(row=1, column=0, padx=10, pady=10)
-password_entry = ttk.Entry(root, show="*")
-password_entry.grid(row=1, column=1, padx=10, pady=10)
+sender_name_label = ttk.Label(form_frame, text="Sender Name:")
+sender_name_label.grid(row=0, column=0, padx=5, pady=5)
+sender_name_entry = ttk.Entry(form_frame)
+sender_name_entry.grid(row=0, column=1, padx=5, pady=5)
 
-show_password_var = tk.IntVar()
-show_password_checkbox = ttk.Checkbutton(root, text="Show Password", variable=show_password_var,
-                                         command=lambda: password_entry.configure(
-                                             show='*' if not show_password_var.get() else ''))
-show_password_checkbox.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+username_label = ttk.Label(form_frame, text="Email:")
+username_label.grid(row=1, column=0, padx=5, pady=5)
+username_entry = ttk.Entry(form_frame)
+username_entry.grid(row=1, column=1, padx=5, pady=5)
 
-to_email_label = ttk.Label(root, text="To Email(s):")
-to_email_label.grid(row=3, column=0, padx=10, pady=10)
-to_email_entry = ttk.Entry(root)
-to_email_entry.grid(row=3, column=1, padx=10, pady=10)
+password_label = ttk.Label(form_frame, text="Password:")
+password_label.grid(row=2, column=0, padx=5, pady=5)
+password_entry = ttk.Entry(form_frame, show="*")
+password_entry.grid(row=2, column=1, padx=5, pady=5)
 
-subject_label = ttk.Label(root, text="Subject:")
-subject_label.grid(row=4, column=0, padx=10, pady=10)
-subject_entry = ttk.Entry(root)
-subject_entry.grid(row=4, column=1, padx=10, pady=10)
+to_email_label = ttk.Label(form_frame, text="To Email(s):")
+to_email_label.grid(row=3, column=0, padx=5, pady=5)
+to_email_entry = ttk.Entry(form_frame)
+to_email_entry.grid(row=3, column=1, padx=5, pady=5)
 
-body_label = ttk.Label(root, text="Body:")
-body_label.grid(row=5, column=0, padx=10, pady=10)
-body_entry = tk.Text(root)
-body_entry.grid(row=5, column=1, padx=10, pady=10)
+subject_label = ttk.Label(form_frame, text="Subject:")
+subject_label.grid(row=4, column=0, padx=5, pady=5)
+subject_entry = ttk.Entry(form_frame)
+subject_entry.grid(row=4, column=1, padx=5, pady=5)
 
-submit_button = ttk.Button(root, text="Send Email", command=submit_form)
-submit_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
+body_label = ttk.Label(form_frame, text="Body:")
+body_label.grid(row=5, column=0, padx=5, pady=5)
+body_entry = tk.Text(form_frame, height=10, width=40)
+body_entry.grid(row=5, column=1, padx=5, pady=5, sticky='ew')
+
+submit_button = ttk.Button(button_frame, text="Send Email", command=submit_form)
+submit_button.pack(side=tk.LEFT, padx=5, pady=5, expand=True)
 
 save_credentials_var = tk.IntVar()
-save_credentials_checkbox = ttk.Checkbutton(root, text="Save Credentials", variable=save_credentials_var)
-save_credentials_checkbox.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+save_credentials_checkbox = ttk.Checkbutton(button_frame, text="Save Credentials", variable=save_credentials_var)
+save_credentials_checkbox.pack(side=tk.RIGHT, padx=5, pady=5, expand=True)
 
 load_credentials()
 
